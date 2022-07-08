ad3dbae26cf3e0ce6ab806de680d5587a493597a
jocan <eljcarlos2n@gmail.com>
2022-07-08 14:25:17 +0200

    Test passed

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,22 @@
 function geekshubs(index) {
-    return "";
+
+     let result = toString();
+    
+    for (let i = 0; i <= index; i++) {
+        if (i%3==0  && i%5 == 0) {
+            result=result+'GeeksHubs\n'
+        }else if (i == 1) {
+            result = "1\n";
+        }else if (i%5==0) {
+            result=result+"Hubs\n"
+        }else if (i%3 == 0) {
+            result=result+'Geeks\n'
+        }else {
+            result=result+i+'\n'
+        }
+        
+        
+    }
+    return result;
 }
 module.exports = geekshubs;
